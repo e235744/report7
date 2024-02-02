92a72c4e1a68ecb0b6820ed319f743d39647dbe2
e235744 <e235744@ie.u-ryukyu.ac.jp>
2024-02-02 19:12:42 +0900

    五つの要素の時

---
+++ Practice.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void setArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int t = i + 1; t < size; t++) {
+            int x;
+            // array[i] の中身と array[t] の中身を交換するために一度 x に入れる
+            if (array[i] < array[t]) {
+                x = array[i];
+                array[i] = array[t];
+                array[t] = x;
+            }
+        }
+    }
+}
+
+int main() {
+    const int SIZE =5; //定数として配列のサイズを宣言                          
+    int scores[SIZE] = {0, 60, 70, 100, 90,};
+    //処理コード
+    setArray(scores, SIZE);
+    for (int i = 0; i < SIZE; i++) {
+        printf("scores[%d] = %d\n", i, scores[i]);
+    }
+    return 0;
+}
